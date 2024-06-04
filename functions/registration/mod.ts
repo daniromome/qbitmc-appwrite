df4f07e9146e086b3227c4a8040204a599b1fd47
Dani Romo <dev@daniromo.me>
2024-06-04 08:59:59 -0600

    Properly handle profile registration

--- functions/registration/mod.ts
+++ functions/registration/mod.ts
@@ -1,43 +1,42 @@
-import { Account, Client } from 'https://deno.land/x/appwrite@11.0.0/mod.ts'
-import { RESTGetAPICurrentUserResult } from 'https://deno.land/x/discord_api_types/v10.ts';
+import { Account, Client, Databases } from 'https://deno.land/x/appwrite@11.0.0/mod.ts'
+import { RESTGetAPICurrentUserResult } from "https://deno.land/x/discord_api_types@0.37.87/v10.ts";
 
 export default async ({ req, res, log, error }: any) => {
   const project = Deno.env.get('APPWRITE_FUNCTION_PROJECT_ID')
-  // const key = Deno.env.get('APPWRITE_API_KEY')
-  // const database = Deno.env.get('DATABASE_ID')
-  // const collection = Deno.env.get('DATABASE_ENROLLMENT_ID')
+  const database = Deno.env.get('APPWRITE_DATABASE_ID')
+  const collection = Deno.env.get('APPWRITE_COLLECTION_PROFILE')
+  const env = Deno.env.get('ENV') || 'dev'
   if (!project) error('Appwrite project environment variable is not defined')
-  //if (!key) error('Appwrite key environment variable is not defined')
-  // if (!database) error('Database id environment variable is not defined')
-  //  if (!collection) error('Collection id environment variable is not defined')
-  if (!project)  return res.empty()
+  if (!database) error('Database id environment variable is not defined')
+  if (!collection) error('Collection id environment variable is not defined')
+  if (!project || !database || !collection)  return res.empty()
+
   const userClient = new Client()
     .setEndpoint('https://appwrite.qbitmc.com/v1')
     .setProject(project)
     .setJWT(req.headers['x-appwrite-user-jwt'])
 
+  const databases = new Databases(userClient)
+
   const account = new Account(userClient)
 
-  const session = await account.getSession('current')
+  const user = await account.get()
 
-  log('provider', session.provider)
-  log('token', session.providerAccessToken)
-
-  const discordResponse = await fetch(
-    'https://discord.com/api/v10/users/@me',
-    { headers: { authorization: `Bearer ${session.providerAccessToken}` }, method: 'GET' }
-  )
-
-  const discordUser = (await discordResponse.json()) as RESTGetAPICurrentUserResult
-  log('locale', discordUser.locale)
-  log('username', discordUser.id)
-
-  return res.empty()
-
-  // const client = new Client()
-  //   .setEndpoint('https://appwrite.qbitmc.com/v1')
-  //   .setProject(project)
-  //   .setKey(key)
-
-  // const databases = new Databases(client)
+  try {
+    const profile = await databases.getDocument(database, collection, user.$id)
+    return res.json(profile)
+  } catch (_) {
+    const discordResponse = await fetch(
+      'https://discord.com/api/v10/users/@me',
+      { headers: { authorization: `Bearer ${req.accessToken}` }, method: 'GET' }
+    )
+    const discordUser = (await discordResponse.json()) as RESTGetAPICurrentUserResult
+    if (env === 'dev') {
+      log('User\'s locale:', discordUser.locale)
+      log('User\'s discord id:', discordUser.id)
+    }
+    if (discordUser.locale) await account.updatePrefs({ ...account.getPrefs(), locale: discordUser.locale })
+    const profile = await databases.createDocument(database, collection, user.$id, { discord: discordUser.id })
+    return res.json(profile)
+  }
 }
