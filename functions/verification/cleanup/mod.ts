40def704853696765e9aad01d35bb13c4229f42a
Dani Romo <dev@daniromo.me>
2024-06-08 00:38:00 -0600

    Code cleanup

--- functions/verification/cleanup/mod.ts
+++ functions/verification/cleanup/mod.ts
@@ -1,13 +1,12 @@
 import { Client, Databases, Query } from 'https://deno.land/x/appwrite@11.0.0/mod.ts'
 
 // deno-lint-ignore no-explicit-any
-export default async ({ req, res, log, _error }: any) => {
+export default async ({ _req, res, log, _error }: any) => {
   const endpoint = Deno.env.get('APPWRITE_ENDPOINT')
   const project = Deno.env.get('APPWRITE_FUNCTION_PROJECT_ID')
   const key = Deno.env.get('APPWRITE_API_KEY')
   const database = Deno.env.get('APPWRITE_DATABASE_ID')
   const collection = Deno.env.get('APPWRITE_COLLECTION_VERIFICATION')
-  const env = Deno.env.get('ENV') || 'dev'
     
   if (!endpoint) throw new Error('Appwrite endpoint environment variable is not defined') 
   if (!project) throw new Error('Appwrite project environment variable is not defined')
@@ -24,5 +23,6 @@ export default async ({ req, res, log, _error }: any) => {
   const documentList = await databases.listDocuments(database, collection, [Query.lessThan('expires', new Date().toISOString())])
   if (!documentList) return res.empty()
   await Promise.allSettled(documentList.documents.map(d => databases.deleteDocument(database, collection, d.$id)))
-  return res.json({ deleted: documentList.total })
+  log(`Deleted ${documentList.total} expired verifications`)
+  return res.empty()
 }
