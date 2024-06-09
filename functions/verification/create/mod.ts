0604e8b237494029bf068adbfbbc1264d35d76cf
Dani Romo <dev@daniromo.me>
2024-06-09 09:02:59 -0600

    Added security and fixed found player logic

--- functions/verification/create/mod.ts
+++ functions/verification/create/mod.ts
@@ -8,6 +8,7 @@ export default async ({ req, res, log, error }: any) => {
   const database = Deno.env.get('APPWRITE_DATABASE_ID')
   const verificationCollection = Deno.env.get('APPWRITE_COLLECTION_VERIFICATION')
   const playerCollection = Deno.env.get('APPWRITE_COLLECTION_PLAYER')
+  const allowed = Deno.env.get('ALLOWED_IP')
     
   if (!endpoint) throw new Error('Appwrite endpoint environment variable is not defined') 
   if (!project) throw new Error('Appwrite project environment variable is not defined')
@@ -16,7 +17,9 @@ export default async ({ req, res, log, error }: any) => {
   if (!verificationCollection) throw new Error('Verification collection id environment variable is not defined')
   if (!playerCollection) throw new Error('Player collection id environment variable is not defined')
 
-  log(JSON.stringify(req.headers))
+  const allowedIps = allowed ? allowed.split(',') : []
+  const ips = (req.headers['x-forwarded-for'] as string).split(',')
+  if (!allowedIps.some(i => ips.includes(i))) throw new Error('Unauthorized')
 
   const { uuid, name, expires } = req.body as { uuid: string; name: string; expires: string }
   if (!uuid || !name || !expires) throw new Error('Bad Request')
@@ -29,9 +32,9 @@ export default async ({ req, res, log, error }: any) => {
 
   try {
     await databases.getDocument(database, playerCollection, uuid)
+    return res.json({ code: null, alreadyLinked: true })
   } catch (e) {
     error(e)
-    return res.json({ code: null, alreadyLinked: true })
   }
 
   const code = Array.from(Array(6)).map(() => Math.floor(Math.random() * 10)).join('')
