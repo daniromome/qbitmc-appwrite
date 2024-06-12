4e0982ec55c33d82fca8a6216868e8a8af6bb158
Dani Romo <dev@daniromo.me>
2024-06-12 17:26:13 -0600

    Defer interaction response

--- functions/discord/interaction/mod.ts
+++ functions/discord/interaction/mod.ts
@@ -1,4 +1,4 @@
-import { verifySignature, Interaction, InteractionResponseTypes, InteractionTypes, createBot } from 'https://deno.land/x/discordeno@18.0.1/mod.ts'
+import { verifySignature, Interaction, InteractionResponseTypes, InteractionTypes } from 'https://deno.land/x/discordeno@18.0.1/mod.ts'
 import { camelize } from 'https://deno.land/x/camelize@2.0.0/mod.ts'
 import { getHandler } from './models/handler.ts'
 import { loadEnvironment } from 'jsr:@qbitmc/deno@0.0.3/appwrite'
@@ -38,11 +38,8 @@ export default async ({ req, res, _log, _error }: any) => {
 
   const message = await handler.handle(environment, payload)
 
-  const bot = createBot({ token: environment.discord.token })
-  await bot.helpers.sendMessage(payload.channelId!, { content: message })
-  return res.empty()
-  // return res.json({
-  //   type: InteractionResponseTypes.ChannelMessageWithSource,
-  //   data: { content: message }
-  // }, 200)
+  return res.json({
+    type: InteractionResponseTypes.DeferredUpdateMessage,
+    data: { content: message }
+  }, 200)
 }
